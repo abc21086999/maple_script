42046fbdce55fd2a5a10cdf4f100ec18a7ba151e
abc21086999 <abc21086999@gmail.com>
2024-08-11 12:41:40 +0800

    Adjust the movement function

--- main.py
+++ main.py
@@ -51,12 +51,14 @@ def main():
             pydirectinput.press(queue.popleft())
             time.sleep(random.choice(wait_time))
 
-        # 一個隨機移動的功能
-        if not character_position:
-            character_position = jump_to_right()
+        # 偵測是不是在桃源境的功能
+        if is_ready("photos/shangri_la.png"):
+            # 一個隨機移動的功能
+            if not character_position:
+                character_position = jump_to_right()
 
-        elif character_position:
-            character_position = jump_to_left()
+            elif character_position:
+                character_position = jump_to_left()
 
 
 if __name__ == "__main__":
