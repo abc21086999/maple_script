c84ea72c7cc1dd36ac6705e9aa38f49d9ec9e054
abc21086999 <abc21086999@gmail.com>
2024-08-16 23:40:44 +0800

    Add some note

--- complete_aut_missions.py
+++ complete_aut_missions.py
@@ -1,35 +1,32 @@
 from functions import *
-from collections import deque
-import random
 import pydirectinput
-import pyautogui
 
 
 # 切換到楓之谷的程式
 switch_to_maple()
 
 
-# 根據有沒有找到來決定要放哪個技能
-def main():
+# 自動完成AUT的任務
+def aut_main():
     while True:
+        # 移動到角色ID作為緩衝
         move_mouse("photos/name.png")
+        # 點擊黃色燈泡
         move_mouse_and_click("photos/light_bulb.png")
+        # 如果有已經完成的任務
         if is_ready("photos/complete.png"):
+            # 最多按三次Y
             for i in range(3):
                 if is_ready("photos/next.png"):
                     pydirectinput.press("y")
+            # 最多按三次『是』
             for i in range(3):
                 if is_ready("photos/yes.png"):
                     move_mouse_and_click("photos/yes.png")
+        # 沒有已經完成的任務，跳出迴圈
         else:
             break
-#    點黃色燈泡
-#    按下任務
-#    再按一次
-#    尋找是的位置，然後將滑鼠移動過去
-#    點下去
-#    重複一次
 
 
 if __name__ == "__main__":
-    main()
+    aut_main()
