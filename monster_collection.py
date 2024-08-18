74d97e80fb1dc70f744a0034cb371525a15e5471
abc21086999 <abc21086999@gmail.com>
2024-08-18 12:19:27 +0800

    Refine for monster_collection.py and complete_aut_missions.py

--- monster_collection.py
+++ monster_collection.py
@@ -17,18 +17,22 @@ def monster_main():
     move_mouse_and_click("photos/monster_collection.png", confidence=0.6)
     # 點擊探險那個tab
     move_mouse_and_click("photos/adventure_tab.png")
-    for i in range(5):
-        # 點擊完成蒐藏
-        move_mouse_and_click("photos/receive_monster_collection_reward.png")
-        time.sleep(0.3)
-        pydirectinput.press("enter")
-    for i in range(5):
-        # 重新開始探險
-        move_mouse_and_click("photos/start_adventure.png")
-        move_mouse_and_click("photos/confirm.png")
-        move_mouse_and_click("photos/monster_collention_title.png")
-    # 完成後關閉視窗
-    pydirectinput.press("esc")
+    time.sleep(0.3)
+    while True:
+        if is_ready("photos/receive_monster_collection_reward.png"):
+                # 點擊完成蒐藏
+                move_mouse_and_click("photos/receive_monster_collection_reward.png")
+                time.sleep(0.3)
+                pydirectinput.press("enter")
+                # 重新開始探險
+                move_mouse_and_click("photos/monster_collention_title.png")
+                move_mouse_and_click("photos/start_adventure.png")
+                move_mouse_and_click("photos/confirm.png")
+                move_mouse_and_click("photos/monster_collention_title.png")
+        else:
+            # 完成後關閉視窗
+            pydirectinput.press("esc")
+            break
 
 
 if __name__ == "__main__":
