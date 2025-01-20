70c19b5b04ca80f92caa16114df328cb66326397
abc21086999 <abc21086999@gmail.com>
2025-01-20 22:59:36 +0800

    refactor the aut complete_aut_missions.py

--- complete_aut_missions.py
+++ complete_aut_missions.py
@@ -2,37 +2,13 @@ from functions import *
 import pydirectinput
 
 
-# 切換到楓之谷的程式
-switch_to_maple()
-
-
 # 自動完成AUT的任務
 def aut_main():
-    while True:
-        # 移動到角色ID作為緩衝
-        move_mouse("photos/name.png")
-        # 點擊黃色燈泡
-        move_mouse_and_click("photos/light_bulb.png")
-        # 如果有已經完成的任務
-        if is_ready("photos/complete.png"):
-            # 最多按三次Y
-            for i in range(3):
-                if is_ready("photos/next.png"):
-                    pydirectinput.press("y")
-            # 最多按三次『是』
-            for i in range(4):
-                if is_ready("photos/yes.png"):
-                    pydirectinput.press("right")
-                    time.sleep(0.1)
-                    pydirectinput.press("y")
-                    time.sleep(0.1)
-                    pydirectinput.press("enter")
-            pydirectinput.press("y")
-            time.sleep(0.1)
-        # 沒有已經完成的任務，跳出迴圈
-        else:
-            pydirectinput.press("esc")
-            break
+    # 切換到楓之谷的程式
+    switch_to_maple()
+    move_mouse_and_click('photos/schedule.png')
+    move_mouse_and_click('photos/schedule_daily_all_complete.png')
+    move_mouse_and_click('photos/schedule_panel_confirm.png')
 
 
 if __name__ == "__main__":
