a236324d1deda7df554fd23cb281e1d90cca6b70
abc21086999 <abc21086999@gmail.com>
2025-07-03 21:09:07 +0800

    Revolutionarily refine the way we deal with windows focus issue

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from functions import *
 
 def main():
     # 切換到楓之谷的程式
-    switch_to_maple()
+    maple = switch_to_maple()
 
     while True:
-        queue = skill_ready()
-        press_ready_skill(queue, min_sec=0.1, max_sec=1)
-        move_by_pressing_up()
-        if monster_collected():
-            break
+        if maple.isActive:
+            queue = skill_ready(maple)
+            press_ready_skill(maple, queue, min_sec=0.1, max_sec=1)
+            move_by_pressing_up()
+        else:
+            continue
 
 
 if __name__ == "__main__":
