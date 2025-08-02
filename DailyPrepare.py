5e383c197d4ee44266a72b7c4ace0d3fba961bed
abc21086999 <abc21086999@gmail.com>
2025-08-03 01:39:50 +0800

    Update path related code

--- DailyPrepare.py
+++ DailyPrepare.py
@@ -15,15 +15,15 @@ class DailyPrepare(MapleScript):
         time.sleep(0.3)
 
         # 移動過去練功的圖案，並且點下去
-        self.find_and_click_image("photos/daily_set.png")
+        self.find_and_click_image(self.get_photo_path("daily_set.png"))
         time.sleep(0.3)
 
         # 移動過去套用按鈕，並且點下去
-        self.find_and_click_image("photos/daily_apply.png")
+        self.find_and_click_image(self.get_photo_path("daily_apply.png"))
         time.sleep(0.3)
 
         # 如果有東西不一樣（出現確認按鈕）需要切換，再按下Enter
-        confirm_pop_up = PIL.Image.open("photos/confirm_daily_pop_up.png")
+        confirm_pop_up = PIL.Image.open(self.get_photo_path("confirm_daily_pop_up.png"))
         cur_ss = self.get_full_screen_screenshot()
         if self.is_on_screen(confirm_pop_up, cur_ss):
             self.press("enter")
@@ -38,7 +38,7 @@ class DailyPrepare(MapleScript):
         time.sleep(0.7)
 
         # 按下拿硬幣的按鈕，接著用Enter關掉確認界面
-        self.find_and_click_image("photos/get_coin.png")
+        self.find_and_click_image(self.get_photo_path("get_coin.png"))
         time.sleep(0.5)
         self.press("enter")
         self.press("esc")
