189a6cc4ce35942634531d1df95a8ee065045c49
abc21086999 <abc21086999@gmail.com>
2025-08-03 22:56:36 +0800

    Add start the monster collection

--- MonsterCollection.py
+++ MonsterCollection.py
@@ -8,19 +8,21 @@ class MonsterCollection(MapleScript):
     def __init__(self, controller=None):
         super().__init__(controller=controller)
 
-    def collect_monster_collection(self):
+    def collect_and_start_monster_collection(self):
         # 打開怪物蒐藏界面
         self.press("f10")
         time.sleep(0.5)
 
         # 按下探險的那個Tab
-        self.find_and_click_image(self.get_photo_path("adventure_tab.png"))
+        adventure_tab = self.get_photo_path("adventure_tab.png")
+        self.find_and_click_image(adventure_tab)
         time.sleep(0.3)
 
         # 一直截圖進行辨識，如果畫面上還有還沒領取的怪物蒐藏
-        while self.is_on_screen(self.get_photo_path("receive_monster_collection_reward.png"), self.get_full_screen_screenshot()):
+        reward_button = self.get_photo_path("receive_monster_collection_reward.png")
+        while self.is_on_screen(reward_button):
             # 按下領取按鈕
-            self.find_and_click_image(self.get_photo_path("receive_monster_collection_reward.png"))
+            self.find_and_click_image(reward_button)
             time.sleep(0.2)
 
             # 按下Enter關閉
@@ -28,8 +30,19 @@ class MonsterCollection(MapleScript):
             time.sleep(0.2)
 
             # 滑鼠回到Tab避免干擾辨識
-            self.find_and_click_image(self.get_photo_path("adventure_tab.png"))
+            self.find_and_click_image(adventure_tab)
             time.sleep(0.2)
+
+        # 一直截圖進行辨識，如果畫面上還有還沒開始的怪物蒐藏，就開始進行蒐藏
+        start_button = self.get_photo_path("start_adventure.png")
+        while self.is_on_screen(start_button):
+            # 按下開始按鈕
+            self.find_and_click_image(start_button)
+            time.sleep(0.35)
+
+            # 按下Enter關閉
+            self.press("enter")
+            time.sleep(0.35)
 
         # 最後用Esc將怪物蒐藏界面關閉
         self.press("esc")
@@ -38,4 +51,4 @@ class MonsterCollection(MapleScript):
 if __name__ == "__main__":
     with XiaoController() as controller:
         Maple = MonsterCollection(controller)
-        Maple.collect_monster_collection()
+        Maple.collect_and_start_monster_collection()
