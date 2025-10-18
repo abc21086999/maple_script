5ea629320e48f97c2c9132ab59278f1a1ea5fdc8
abc21086999 <abc21086999@gmail.com>
2025-10-18 21:54:58 +0800

    Add a function to get character position and refine the loop flow

--- MapleGrind.py
+++ MapleGrind.py
@@ -8,7 +8,7 @@ class MapleGrind(MapleScript):
 
     def __init__(self, controller=None):
         super().__init__(controller=controller)
-        self.__position = "left"
+        self.__position = self.get_character_position()
         self.__skills_list = list()
         self.__gap_time = (0.5, 1.0)
         self.__skills_dict = {
@@ -83,6 +83,25 @@ class MapleGrind(MapleScript):
                 "image": PIL.Image.open(self.get_photo_path("anemoi.png")),
             }
         }
+        self.__left_loop = \
+            [('press', 'down', 1.93), ('press', 'alt', 2.02), ('release', 'alt', 2.24), ('release', 'down', 2.29),
+             ('press', 'alt', 2.95), ('release', 'alt', 3.06), ('press', 'alt', 3.14), ('release', 'alt', 3.28),
+             ('press', 'alt', 3.96), ('release', 'alt', 4.07), ('press', 'alt', 4.2), ('release', 'alt', 4.34),
+             ('press', 'alt', 5.16), ('release', 'alt', 5.25), ('press', 'alt', 5.37), ('release', 'alt', 5.48),
+             ('press', 'alt', 6.31), ('release', 'alt', 6.39), ('press', 'alt', 6.5), ('release', 'alt', 6.63),
+             ('press', 'alt', 7.61), ('release', 'alt', 7.74), ('press', 'up', 7.84), ('press', 'alt', 8.12),
+             ('release', 'alt', 8.31), ('release', 'up', 8.41), ('press', 'left', 9.41), ('release', 'left', 9.47)]
+        self.__right_loop = \
+            [('press', 'alt', 2.07), ('release', 'alt', 2.21), ('press', 'up', 2.22), ('press', 'alt', 2.37),
+             ('release', 'alt', 2.58), ('release', 'up', 2.66), ('press', 'alt', 3.48), ('release', 'alt', 3.57),
+             ('press', 'alt', 3.88), ('release', 'alt', 3.99), ('press', 'alt', 4.99), ('release', 'alt', 5.11),
+             ('press', 'alt', 5.27), ('release', 'alt', 5.45), ('press', 'alt', 6.22), ('release', 'alt', 6.35),
+             ('press', 'alt', 6.54), ('release', 'alt', 6.7), ('press', 'alt', 7.71), ('release', 'alt', 7.82),
+             ('press', 'alt', 7.96), ('release', 'alt', 8.11), ('press', 'down', 8.94), ('press', 'alt', 8.97),
+             ('release', 'alt', 9.11), ('release', 'down', 9.18), ('press', 'down', 9.96), ('press', 'alt', 9.99),
+             ('release', 'alt', 10.14), ('release', 'down', 10.21), ('press', 'right', 10.81),
+             ('release', 'right', 10.85)]
+        self.__loop_map = {"left": self.__left_loop, "right": self.__right_loop, "not_found": []}
 
 
     def find_ready_skill(self) -> None:
@@ -157,35 +176,17 @@ class MapleGrind(MapleScript):
         """
         根據錄製的腳本來重播操作
         """
-
-        recorded_events_left = \
-            [('press', 'down', 1.93), ('press', 'alt', 2.02), ('release', 'alt', 2.24), ('release', 'down', 2.29),
-             ('press', 'alt', 2.95), ('release', 'alt', 3.06), ('press', 'alt', 3.14), ('release', 'alt', 3.28),
-             ('press', 'alt', 3.96), ('release', 'alt', 4.07), ('press', 'alt', 4.2), ('release', 'alt', 4.34),
-             ('press', 'alt', 5.16), ('release', 'alt', 5.25), ('press', 'alt', 5.37), ('release', 'alt', 5.48),
-             ('press', 'alt', 6.31), ('release', 'alt', 6.39), ('press', 'alt', 6.5), ('release', 'alt', 6.63),
-             ('press', 'alt', 7.61), ('release', 'alt', 7.74), ('press', 'up', 7.84), ('press', 'alt', 8.12),
-             ('release', 'alt', 8.31), ('release', 'up', 8.41), ('press', 'left', 9.41), ('release', 'left', 9.47)]
-
-        recorded_events_right = \
-            [('press', 'alt', 2.07), ('release', 'alt', 2.21), ('press', 'up', 2.22), ('press', 'alt', 2.37),
-             ('release', 'alt', 2.58), ('release', 'up', 2.66), ('press', 'alt', 3.48), ('release', 'alt', 3.57),
-             ('press', 'alt', 3.88), ('release', 'alt', 3.99), ('press', 'alt', 4.99), ('release', 'alt', 5.11),
-             ('press', 'alt', 5.27), ('release', 'alt', 5.45), ('press', 'alt', 6.22), ('release', 'alt', 6.35),
-             ('press', 'alt', 6.54), ('release', 'alt', 6.7), ('press', 'alt', 7.71), ('release', 'alt', 7.82),
-             ('press', 'alt', 7.96), ('release', 'alt', 8.11), ('press', 'down', 8.94), ('press', 'alt', 8.97),
-             ('release', 'alt', 9.11), ('release', 'down', 9.18), ('press', 'down', 9.96), ('press', 'alt', 9.99),
-             ('release', 'alt', 10.14), ('release', 'down', 10.21), ('press', 'right', 10.81),
-             ('release', 'right', 10.85)]
-
-        start_replay_time = time.time()
-        if self.__position == "left":
-            recorded_events = recorded_events_left
-        else:
-            recorded_events = recorded_events_right
-        if random.random() < 0.1:
+        # 機率小於10％再來跑圖
+        if random.random() < 0.05 and self.is_maple_focus():
             print("開始使用紀錄的腳本")
+
+            recorded_events = self.__loop_map.get(self.get_character_position(), [])
+
+            start_replay_time = time.time()
             for action, key_str, event_time in recorded_events:
+                if not self.is_maple_focus():
+                    print("楓之谷不在前景，中止跑圖")
+                    break
                 target_time = start_replay_time + event_time
                 sleep_duration = target_time - time.time()
                 if sleep_duration > 0:
@@ -196,12 +197,7 @@ class MapleGrind(MapleScript):
                 elif action == 'release':
                     self.key_up(key_str)
 
-            if recorded_events == recorded_events_left:
-                self.__position = "right"
-            elif recorded_events == recorded_events_right:
-                self.__position = "left"
-
-            print("腳本重播完畢。")
+            print("腳本重播完畢")
 
     def start(self) -> None:
         try:
