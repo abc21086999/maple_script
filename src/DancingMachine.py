adbc11fbf87133bc574c0e4f1e64614b4779d8c9
abc21086999 <abc21086999@gmail.com>
2025-11-17 23:47:10 +0800

    Optimize the dancing flow

--- src/DancingMachine.py
+++ src/DancingMachine.py
@@ -12,14 +12,16 @@ class Dancing(MapleScript):
 
 
     def start(self):
-        while not self.is_on_screen(pic=self.get_photo_path("") / "dancing" / "end_npc.png"):
+        while True:
             sc = self.get_full_screen_screenshot()
+            if self.is_on_screen(pic=self.get_photo_path("") / "dancing" / "end_npc.png"):
+                break
             for direction, pic in self.direction_dict.items():
                 if self.is_on_screen(pic=pic, img=sc):
                     self.ans_list.append(direction)
                     print(direction)
                     time.sleep(1)
-            if self.is_on_screen(pic=self.get_photo_path("") / "dancing" / "end.png"):
+            if self.is_on_screen(pic=self.get_photo_path("") / "dancing" / "end.png", img=sc):
                 print(self.ans_list)
                 self.press_and_wait(self.ans_list, wait_time=0.8)
                 self.ans_list.clear()
