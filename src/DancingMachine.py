272f86c7597ce4dbade88690be9f2590d6faf8bf
abc21086999 <abc21086999@gmail.com>
2025-11-17 23:43:03 +0800

    Init the dancing machine

---
+++ src/DancingMachine.py
@@ -0,0 +1,31 @@
+import time
+from src.MapleScript import MapleScript
+from src.XiaoController import XiaoController
+
+
+class Dancing(MapleScript):
+
+    def __init__(self, controller=None):
+        super().__init__(controller=controller)
+        self.direction_dict = self.yaml_loader.dancing_dict
+        self.ans_list = list()
+
+
+    def start(self):
+        while not self.is_on_screen(pic=self.get_photo_path("") / "dancing" / "end_npc.png"):
+            sc = self.get_full_screen_screenshot()
+            for direction, pic in self.direction_dict.items():
+                if self.is_on_screen(pic=pic, img=sc):
+                    self.ans_list.append(direction)
+                    print(direction)
+                    time.sleep(1)
+            if self.is_on_screen(pic=self.get_photo_path("") / "dancing" / "end.png"):
+                print(self.ans_list)
+                self.press_and_wait(self.ans_list, wait_time=0.8)
+                self.ans_list.clear()
+
+
+if __name__ == "__main__":
+    with XiaoController() as Xiao:
+        Maple = Dancing(Xiao)
+        Maple.start()
